84f3cfd4dc6d5d8c85e84c3980f69a6d269f77e8
Yash <yashrajmani1709@gmail.com>
2026-08-12 23:46:43 +0530

    progress

---
+++ Conditionals and loops/Assignment/AreaOfRectangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AreaOfRectangle {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Length: ");
+        double Length = sc.nextDouble();
+        System.out.println("Breadth: ");
+        double Breadth = sc.nextDouble();
+        double area = Length * Breadth;
+         
+    }
+    
+}
